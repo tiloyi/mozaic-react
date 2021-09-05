b7b4e98cea7b367bf2438f3880cbbf949fe711f7
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-09-05 20:42:05 +0500

    update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,14 @@
-export default [];
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+
+export default [
+    {
+        input: ['src/index.ts'],
+        output: {
+            exports: 'named',
+            dir: 'lib'
+        },
+        plugins: [resolve(), commonjs(), typescript()]
+    }
+];
