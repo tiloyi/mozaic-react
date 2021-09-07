0f4e87d4cb246e69a671935d003566e7962139ed
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-09-07 21:35:55 +0500

    apply mozaic styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,11 @@ import { IButtonProps } from './Button.types';
 import './Button.scss';
 
 const Button: FC<IButtonProps> = ({ children, ...props }) => {
-    return <button {...props}>{children}</button>;
+    return (
+        <button className="mc-button" {...props}>
+            {children}
+        </button>
+    );
 };
 
 export default Button;
