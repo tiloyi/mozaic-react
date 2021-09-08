cb0104efa73695044e0b6adec2ade5a0a3f5b27b
abrosimov <abrosimov@byndyusoft.com>
2021-09-08 16:41:07 +0500

    add types

---
+++ src/components/Radio/Radio.types.ts
@@ -0,0 +1,7 @@
+import React, { InputHTMLAttributes } from 'react';
+
+type TOmittedProps = 'checked' | 'disabled';
+
+export interface IRadioProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export interface IRadioContainerProps {}
