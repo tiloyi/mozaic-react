e9919aa970e85257cd6d3e66fd156409cf6c3b49
abrosimov <abrosimov@byndyusoft.com>
2021-09-09 17:40:44 +0500

    add reexports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Button } from './Button';
 export { default as Flag } from './Flag';
+export { default as Radio } from './Radio';
+export { RadioGroup, useRadioGroup } from './RadioGroup';
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { default as Button } from './components/Button';
 export { default as Flag } from './components/Flag';
+export { default as Radio } from './components/Radio';
+export { RadioGroup, useRadioGroup } from './components/RadioGroup';
