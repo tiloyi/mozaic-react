e61b9f1f7b2ce5c020fdf74e19fdd41c2d0c0a6f
abrosimov <abrosimov@byndyusoft.com>
2021-09-13 14:36:32 +0500

    add tests

--- src/components/TextArea/TextArea.tests.tsx
+++ src/components/TextArea/TextArea.tests.tsx
@@ -4,8 +4,33 @@ import TextArea from './TextArea';
 
 describe('components/TextArea', () => {
     test('renders correctly', () => {
-        render(<TextArea />);
+        render(<TextArea defaultValue="text" />);
 
-        expect(screen.getByRole('textbox')).toBeInTheDocument();
+        expect(screen.getByRole('textbox')).toHaveDisplayValue('text');
+    });
+
+    test('renders valid correctly', () => {
+        render(<TextArea isValid />);
+
+        expect(screen.getByRole('textbox')).toHaveClass('is-valid');
+    });
+
+    test('renders invalid correctly', () => {
+        render(<TextArea isInvalid />);
+
+        expect(screen.getByRole('textbox')).toHaveClass('is-invalid');
+        expect(screen.getByRole('textbox')).toHaveAttribute('aria-invalid', 'true');
+    });
+
+    test('renders disabled correctly', () => {
+        render(<TextArea isDisabled />);
+
+        expect(screen.getByRole('textbox')).toBeDisabled();
+    });
+
+    test('throws an error if `isValid` and `isInvalid` are specified as true', () => {
+        expect(() => render(<TextArea isInvalid isValid />)).toThrow(
+            'The properties `isValid` and `isInvalid` can not be true in the same time'
+        );
     });
 });
--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -6,6 +6,10 @@ import './TextArea.scss';
 const TextArea: FC<ITextAreaProps> = ({ className, isDisabled, isInvalid, isValid, ...props }: ITextAreaProps) => {
     const textAreaClassName = cn('mc-textarea', className, isInvalid && 'is-invalid', isValid && 'is-valid');
 
+    if (isInvalid && isValid) {
+        throw new Error('The properties `isValid` and `isInvalid` can not be true in the same time');
+    }
+
     return <textarea className={textAreaClassName} {...props} aria-invalid={isInvalid} disabled={isDisabled} />;
 };
 
