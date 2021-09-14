0084c7ab40840a7e24c2948e4f6bcec5ec8acb21
abrosimov <abrosimov@byndyusoft.com>
2021-09-14 15:58:30 +0500

    fix building

--- rollup.config.js
+++ rollup.config.js
@@ -23,6 +23,7 @@ export default [
                 plugins: postcssPlugins,
                 syntax: postcssSyntax,
                 extract: true,
+                minimize: true,
                 use: [
                     [
                         'sass',
