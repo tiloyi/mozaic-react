edb8f66ad484555214d84ef36256622a88c99e51
abrosimov <abrosimov@byndyusoft.com>
2021-09-16 12:19:11 +0500

    fix test suit name

--- src/components/TextArea/TextArea.tests.tsx
+++ src/components/TextArea/TextArea.tests.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import TextArea from './TextArea';
 
-describe('components/TextInput', () => {
+describe('components/TextArea', () => {
     test('renders correctly', () => {
         render(<TextArea defaultValue="text" />);
 
