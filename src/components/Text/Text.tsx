42258d1fd30ffffbc20f054de5be19d97fde8281
abrosimov <abrosimov@byndyusoft.com>
2021-09-24 14:18:02 +0500

    fix text font-face

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -3,12 +3,15 @@ import cn from 'classnames';
 import { ITextProps, TextSize } from './Text.types';
 import './Text.scss';
 
+const blockClassName = 'mt-text';
+
 const Text: FC<ITextProps> = ({ className, children, as = 'span', align, theme, size = TextSize.M, ...props }) => {
     const elementClassName = cn(
+        blockClassName,
         className,
-        align && `mt-text--${align}`,
-        size && `mt-text--${size}`,
-        theme && `mt-text--${theme}`
+        align && `${blockClassName}--${align}`,
+        size && `${blockClassName}--${size}`,
+        theme && `${blockClassName}--${theme}`
     );
 
     return createElement(as, { className: elementClassName, ...props }, children);
