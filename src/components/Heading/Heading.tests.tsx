c84cb1f013214d4154d67d28df27db689c8d8498
abrosimov <abrosimov@byndyusoft.com>
2021-09-30 00:04:53 +0500

    add tests

--- src/components/Heading/Heading.tests.tsx
+++ src/components/Heading/Heading.tests.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { tags, HeadingAlign, HeadingSize } from './Heading.types';
 import Heading from './Heading';
 
 describe('components/Heading', () => {
-    test('renders correctly', () => {
-        render(<Heading>Test</Heading>);
+    test.each(tags)('renders as `%s` tag correctly', tag => {
+        render(<Heading as={tag}>Heading</Heading>);
 
-        expect(screen.getByText('Test')).toBeInTheDocument();
+        expect(screen.getByText('Heading').tagName).toBe(tag.toUpperCase());
     });
+
+    test('renders with default size correctly', () => {
+        render(<Heading>Heading</Heading>);
+
+        expect(screen.getByText('Heading')).toHaveClass(`mt-heading--m`);
+    });
+
+    test.each([HeadingSize.S, HeadingSize.M, HeadingSize.L])('renders with `%s` size correctly', size => {
+        render(<Heading size={size}>Heading</Heading>);
+
+        expect(screen.getByText('Heading')).toHaveClass(`mt-heading--${size}`);
+    });
+
+    test.each([HeadingAlign.Left, HeadingAlign.Center, HeadingAlign.Right])(
+        'renders with `%s` text align correctly',
+        align => {
+            render(<Heading align={align}>Heading</Heading>);
+
+            expect(screen.getByText('Heading')).toHaveClass(`mt-heading--${align}`);
+        }
+    );
 });
