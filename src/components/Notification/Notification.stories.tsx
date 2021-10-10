cbfcf62778d04cb342a842bca0a15df78b6e4ae1
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-10-10 15:37:51 +0500

    update notification stories

--- src/components/Notification/Notification.stories.tsx
+++ src/components/Notification/Notification.stories.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
 import { Story } from '@storybook/react';
-import { NotificationTitle, NotificationMessage, NotificationFooter } from './partials';
 import Notification from './Notification';
 import { INotificationProps, NotificationSize, NotificationTheme } from './Notification.types';
 
-interface IStory extends INotificationProps {
-    title?: string;
-    message?: string;
-}
-
-const Template: Story<IStory> = ({ title = '', message = '', ...props }) => (
-    <Notification {...props}>
-        {title.length > 0 && <NotificationTitle>{title}</NotificationTitle>}
-        {message.length > 0 && <NotificationMessage>{message}</NotificationMessage>}
-    </Notification>
-);
+const Template: Story<INotificationProps> = args => <Notification {...args} />;
 
 export const Controls = Template.bind({});
 
@@ -23,6 +12,14 @@ Controls.args = {
     theme: NotificationTheme.Info,
     size: NotificationSize.M,
     message: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'
+};
+
+Controls.argTypes = {
+    footer: {
+        table: {
+            disable: true
+        }
+    }
 };
 
 export default {
