ca3437236ba9d179831e5c57358109c1f1be0385
abrosimov <abrosimov@byndyusoft.com>
2021-10-12 14:39:09 +0500

    add layer footer

--- src/components/Layer/Layer.types.ts
+++ src/components/Layer/Layer.types.ts
@@ -1,3 +1,9 @@
+import { ButtonHTMLAttributes, HTMLAttributes } from 'react';
+
 export interface ILayerProps {
+    className?: string;
+}
+
+export interface ILayerPartialProps extends HTMLAttributes<HTMLDivElement> {
     className?: string;
 }
