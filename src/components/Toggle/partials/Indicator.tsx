dd1602a0cada693343cf9a7612cd68ec07eb344d
abrosimov <abrosimov@byndyusoft.com>
2021-10-18 18:20:35 +0500

    eslint issues

--- src/components/Toggle/partials/Indicator.tsx
+++ src/components/Toggle/partials/Indicator.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import cn from 'classnames';
 import { IToggleProps } from '../Toggle.types';
 
-const ToggleIndicator = ({ className, isChecked, isDisabled, size, ...props }: IToggleProps) => {
-    return (
-        <input
-            className={cn('mc-toggle__input', className)}
-            {...props}
-            type="checkbox"
-            checked={isChecked}
-            disabled={isDisabled}
-        />
-    );
-};
+const ToggleIndicator = ({ className, isChecked, isDisabled, size, ...props }: IToggleProps): JSX.Element => (
+    <input
+        className={cn('mc-toggle__input', className)}
+        {...props}
+        type="checkbox"
+        checked={isChecked}
+        disabled={isDisabled}
+    />
+);
 
 export default ToggleIndicator;
