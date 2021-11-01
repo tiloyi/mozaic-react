745aeffe23c351300d4c80d1a9909ed28dff4e88
abrosimov <abrosimov@byndyusoft.com>
2021-11-01 17:05:09 +0500

    register modal

--- src/components/Modal/partials/Container.tsx
+++ src/components/Modal/partials/Container.tsx
@@ -1,29 +1,29 @@
 import React, { FC, useEffect } from 'react';
 import Overlay from '../../Overlay';
+import Portal from '../../Portal';
 import { useModalsState, useModals } from '../../ModalsProvider/ModalsContext';
 import { IModalContainerProps } from '../Modal.types';
-import ModalCloseButton from './CloseButton';
 
 const ModalContainer: FC<IModalContainerProps> = ({ children, id }): JSX.Element | null => {
-    // const { register } = useModals();
-    // const modals = useModalsState();
+    const { register } = useModals();
+    const modals = useModalsState();
 
-    // useEffect(() => register(id), [register, id]);
+    useEffect(() => register(id), [register, id]);
 
-    // if (modals[id].isOpen) {
-    return (
-        <>
-            <div className="mc-modal" role="dialog" tabIndex={-1}>
-                <div className="mc-modal__dialog is-open" role="document">
-                    {children}
+    if (modals[id]?.isOpen) {
+        return (
+            <Portal id={`portal-modal-${id}`}>
+                <div className="mc-modal" role="dialog" tabIndex={-1}>
+                    <div className="mc-modal__dialog is-open" role="document">
+                        {children}
+                    </div>
                 </div>
-            </div>
-            <Overlay isVisible />
-        </>
-    );
-    // }
+                <Overlay isVisible />
+            </Portal>
+        );
+    }
 
-    // return null;
+    return null;
 };
 
 export default ModalContainer;
