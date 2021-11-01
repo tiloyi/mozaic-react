e47e541a9864381141ca1ce79fe827c74554bd77
abrosimov <abrosimov@byndyusoft.com>
2021-11-01 18:26:42 +0500

    add animations

--- src/components/Modal/partials/Container.tsx
+++ src/components/Modal/partials/Container.tsx
@@ -1,14 +1,17 @@
 import React, { FC, useEffect } from 'react';
+import cn from 'classnames';
 import Overlay from '../../Overlay';
 import Portal from '../../Portal';
 import { useModalsState, useModals } from '../../ModalsProvider/ModalsContext';
 import { IModalContainerProps } from '../Modal.types';
 
-const ModalContainer: FC<IModalContainerProps> = ({ children, id }): JSX.Element | null => {
+const ModalContainer: FC<IModalContainerProps> = ({ children, id }): JSX.Element => {
     const { register } = useModals();
     const modals = useModalsState();
 
     useEffect(() => register(id), [id]);
+
+    const isOpen = modals[id]?.isOpen;
 
     if (modals[id]?.isOpen) {
         return (
@@ -23,7 +26,16 @@ const ModalContainer: FC<IModalContainerProps> = ({ children, id }): JSX.Element
         );
     }
 
-    return null;
+    return (
+        <Portal id={`portal-modal-${id}`}>
+            <div className="mc-modal" role="dialog" tabIndex={-1}>
+                <div className={cn('mc-modal__dialog', isOpen && 'is-open')} role="document">
+                    {isOpen && children}
+                </div>
+            </div>
+            <Overlay isVisible={isOpen} />
+        </Portal>
+    );
 };
 
 export default ModalContainer;
