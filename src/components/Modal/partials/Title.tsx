10ff20af627a03f997367e3002528f3b45679877
abrosimov <abrosimov@byndyusoft.com>
2021-11-01 17:30:41 +0500

    eslint issue

--- src/components/Modal/partials/Title.tsx
+++ src/components/Modal/partials/Title.tsx
@@ -2,8 +2,7 @@ import { createElement, FC } from 'react';
 import cn from 'classnames';
 import { IModalTitleProps } from '../Modal.types';
 
-const ModalTitle: FC<IModalTitleProps> = ({ className, children, as = 'h2', ...props }): JSX.Element => {
-    return createElement(as, { className: cn('mc-modal__title', className), ...props }, children);
-};
+const ModalTitle: FC<IModalTitleProps> = ({ className, children, as = 'h2', ...props }): JSX.Element =>
+    createElement(as, { className: cn('mc-modal__title', className), ...props }, children);
 
 export default ModalTitle;
