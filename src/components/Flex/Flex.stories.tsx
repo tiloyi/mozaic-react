eec9bee7641697e3a34902951d1067c0251f6816
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-11-14 13:58:13 +0500

    add stories

--- src/components/Flex/Flex.stories.tsx
+++ src/components/Flex/Flex.stories.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Story } from '@storybook/react';
+import { MagicUnit } from '../../constants';
+import Button, { ButtonSize } from '../Button';
+import View from '../View';
 import { IFlexProps } from './Flex.types';
 import Flex from './Flex';
 import './Flex.stories.scss';
 
-const Template: Story<IFlexProps> = args => (
-    <Flex {...args} className="flex-container">
-        <div className="flex-inner">1</div>
-        <div className="flex-inner">2</div>
-    </Flex>
-);
+const Template: Story<IFlexProps> = args => {
+    const [qty, setQty] = useState(3);
 
-export const FlexStory = Template.bind({});
+    const addChild = () => setQty(prevQty => prevQty + 1);
 
-FlexStory.args = {};
+    const removeChild = () => setQty(prevQty => (prevQty > 1 ? prevQty - 1 : prevQty));
+
+    return (
+        <>
+            <Flex {...args} className="flex-container">
+                {Array.from({ length: qty }).map((_: unknown, index: number) => (
+                    <div className="flex-inner" key={`flex-child-${index}`}>
+                        {index}
+                    </div>
+                ))}
+            </Flex>
+            <Flex padding={MagicUnit.MU100}>
+                <Button size={ButtonSize.S} onClick={addChild}>
+                    Add child
+                </Button>
+                <View marginLeft={MagicUnit.MU100}>
+                    <Button size={ButtonSize.S} onClick={removeChild}>
+                        Remove child
+                    </Button>
+                </View>
+            </Flex>
+        </>
+    );
+};
+
+export const Controls = Template.bind({});
+
+Controls.args = {};
 
 export default {
     title: 'Utilities/Flex',
