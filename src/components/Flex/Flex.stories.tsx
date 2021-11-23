0cd7eac85a73f688bef6dbbe4a0120f98bfd4fe2
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-11-24 01:21:40 +0500

    enums to unions

--- src/components/Flex/Flex.stories.tsx
+++ src/components/Flex/Flex.stories.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Story } from '@storybook/react';
-import { MagicUnit } from '../../constants';
-import Button, { ButtonSize } from '../Button';
+import Button from '../Button';
 import View from '../View';
 import { IFlexProps } from './Flex.types';
 import Flex from './Flex';
@@ -10,9 +9,9 @@ import './Flex.stories.scss';
 const Template: Story<IFlexProps> = args => {
     const [qty, setQty] = useState(3);
 
-    const addChild = () => setQty(prevQty => prevQty + 1);
+    const addChild = (): void => setQty(prevQty => prevQty + 1);
 
-    const removeChild = () => setQty(prevQty => (prevQty > 1 ? prevQty - 1 : prevQty));
+    const removeChild = (): void => setQty(prevQty => (prevQty > 1 ? prevQty - 1 : prevQty));
 
     return (
         <>
@@ -23,12 +22,12 @@ const Template: Story<IFlexProps> = args => {
                     </div>
                 ))}
             </Flex>
-            <Flex padding={MagicUnit.MU100}>
-                <Button size={ButtonSize.S} onClick={addChild}>
+            <Flex padding="mu100">
+                <Button size="s" onClick={addChild}>
                     Add child
                 </Button>
-                <View marginLeft={MagicUnit.MU100}>
-                    <Button size={ButtonSize.S} onClick={removeChild}>
+                <View marginLeft="mu100">
+                    <Button size="s" onClick={removeChild}>
                         Remove child
                     </Button>
                 </View>
