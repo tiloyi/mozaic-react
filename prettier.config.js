7c2e0c7a55dc95af5acbaad1fc3e2f1a6368d336
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2021-12-01 17:39:06 +0100

    chore(package): change name in docs and package-lock

--- prettier.config.js
+++ prettier.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    arrowParens: 'avoid',
-    singleQuote: true,
-    tabWidth: 4,
-    trailingComma: 'none',
-    printWidth: 120,
-    overrides: [
-        {
-            files: ['*.scss', '.css'],
-            options: {
-                singleQuote: false
-            }
-        }
-    ]
+  arrowParens: 'avoid',
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  printWidth: 120,
+  overrides: [
+    {
+      files: ['*.scss', '.css'],
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
 };
