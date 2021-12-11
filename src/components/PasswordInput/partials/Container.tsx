6c352e5e6d1946948b91dff622dbbba1932f74f7
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-12-11 23:32:21 +0500

    implements container

--- src/components/PasswordInput/partials/Container.tsx
+++ src/components/PasswordInput/partials/Container.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
+import cn from 'classnames';
 import { IPasswordInputProps } from '../PasswordInput.types';
+import { PasswordInputContextProvider } from '../PasswordInputContext';
 
-const PasswordInputContainer: FC<IPasswordInputProps> = ({ children }): JSX.Element => {
-    return (
-        <div>{ children }</div>
-    );
-};
+const PasswordInputContainer: FC<IPasswordInputProps> = ({ className, children, ...props }): JSX.Element => (
+    <PasswordInputContextProvider {...props}>
+        <div className={cn('mc-password-input', className)}>{children}</div>
+    </PasswordInputContextProvider>
+);
 
 export default PasswordInputContainer;
