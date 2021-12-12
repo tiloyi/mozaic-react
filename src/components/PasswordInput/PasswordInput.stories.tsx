070b4b89552691d778e3f164c6f143ed5383f69d
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-12-12 18:53:29 +0500

    update stories

--- src/components/PasswordInput/PasswordInput.stories.tsx
+++ src/components/PasswordInput/PasswordInput.stories.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Story } from '@storybook/react';
 import { IPasswordInputProps } from './PasswordInput.types';
 import PasswordInput from './PasswordInput';
 
-const Template: Story<IPasswordInputProps> = args => <PasswordInput {...args}/>;
+const Template: Story<IPasswordInputProps> = args => <PasswordInput {...args} />;
 
-export const PasswordInputStory = Template.bind({});
+export const Controls = Template.bind({});
 
-PasswordInputStory.args = {};
+Controls.args = {
+    value: 'password',
+    isDisabled: false,
+    isInvalid: false,
+    isValid: false
+};
+
+const ExampleTemplate: Story<IPasswordInputProps> = args => {
+    const [value, setValue] = useState('');
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>): void => setValue(event.target.value);
+
+    return <PasswordInput {...args} value={value} onChange={handleChange} />;
+};
+
+export const Example = ExampleTemplate.bind({});
+
+Example.args = {
+    placeholder: 'placeholder',
+    isDisabled: false,
+    isInvalid: false,
+    isValid: false
+};
 
 export const argTypes = {};
