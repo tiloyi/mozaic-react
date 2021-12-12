78ee43aca1c701624b31091c099108d9ac3cad6d
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-12-12 20:45:25 +0500

    add tests

--- src/components/PasswordInput/PasswordInput.tests.tsx
+++ src/components/PasswordInput/PasswordInput.tests.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import PasswordInput from './PasswordInput';
+import userEvent from '@testing-library/user-event';
 
 describe('components/PasswordInput', () => {
     test('renders correctly', () => {
-        render(<PasswordInput>Test</PasswordInput>);
+        render(<PasswordInput defaultValue="password" />);
 
-        expect(screen.getByText('Test')).toBeInTheDocument();
+        expect(screen.getByDisplayValue('password')).toHaveAttribute('type', 'password');
+    });
+
+    test('toggles input type', () => {
+        render(<PasswordInput defaultValue="password" />);
+
+        userEvent.click(screen.getByRole('button', { name: 'Show' }));
+
+        expect(screen.getByDisplayValue('password')).toHaveAttribute('type', 'text');
+
+        userEvent.click(screen.getByRole('button', { name: 'Hide' }));
+
+        expect(screen.getByDisplayValue('password')).toHaveAttribute('type', 'password');
+    });
+
+    test('renders disabled correctly', () => {
+        render(<PasswordInput defaultValue="password" isDisabled />);
+
+        expect(screen.getByDisplayValue('password')).toBeDisabled();
     });
 });
