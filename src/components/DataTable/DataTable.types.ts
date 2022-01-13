9316df751882b56d28e6c977ce92c7a447cf21e2
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-01-13 17:17:36 +0500

    add typings

--- src/components/DataTable/DataTable.types.ts
+++ src/components/DataTable/DataTable.types.ts
@@ -1,3 +1,13 @@
+import { HTMLAttributes, TdHTMLAttributes, ThHTMLAttributes } from 'react';
+
 export interface IDataTableProps {
     className?: string;
 }
+
+export interface IDataTableCellProps extends TdHTMLAttributes<HTMLTableCellElement> {}
+
+export interface IDataTableRowProps extends HTMLAttributes<HTMLTableRowElement> {}
+
+export interface IDataTableHeaderRowProps extends HTMLAttributes<HTMLTableRowElement> {}
+
+export interface IDataTableHeaderCellProps extends ThHTMLAttributes<HTMLTableHeaderCellElement> {}
