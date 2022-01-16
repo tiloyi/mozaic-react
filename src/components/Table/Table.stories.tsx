410e00fa8b3a50f468d23b648546f3c6f1a58770
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-01-16 15:12:57 +0500

    add basic story

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Story } from '@storybook/react';
+import { generateDataTableRows } from '../DataTable/DataTable.fixtures';
 import { TableHeader, TableBody, TableRow, TableHeaderCell, TableHeaderLabel, TableCell } from './partials';
 import Table from './Table';
+import Badge from '../Badge';
+
+const rows = generateDataTableRows(10);
 
 const Template: Story = () => (
     <Table>
@@ -10,12 +14,28 @@ const Template: Story = () => (
                 <TableHeaderCell>
                     <TableHeaderLabel>Id</TableHeaderLabel>
                 </TableHeaderCell>
+                <TableHeaderCell>
+                    <TableHeaderLabel>Name</TableHeaderLabel>
+                </TableHeaderCell>
+                <TableHeaderCell>
+                    <TableHeaderLabel>Count</TableHeaderLabel>
+                </TableHeaderCell>
+                <TableHeaderCell>
+                    <TableHeaderLabel>Status</TableHeaderLabel>
+                </TableHeaderCell>
             </TableRow>
         </TableHeader>
         <TableBody>
-            <TableRow>
-                <TableCell>1</TableCell>
-            </TableRow>
+            {rows.map(row => (
+                <TableRow key={`row-${row.id}`}>
+                    <TableCell>{row.id}</TableCell>
+                    <TableCell>{row.name}</TableCell>
+                    <TableCell>{row.count}</TableCell>
+                    <TableCell>
+                        <Badge theme={row.status === 'success' ? 'success' : 'danger'}>{row.status}</Badge>
+                    </TableCell>
+                </TableRow>
+            ))}
         </TableBody>
     </Table>
 );
