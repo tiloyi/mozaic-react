3bc406c8dc10b17653cd59264bb19910b4517c3a
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-01-16 12:40:41 +0500

    implementation & stories

--- src/components/DataTable/DataTable.stories.tsx
+++ src/components/DataTable/DataTable.stories.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Story } from '@storybook/react';
 import { IDataTableProps } from './DataTable.types';
+import { generateDataTableRows, columns, getRowKey, IDataTableFixture } from './DataTable.fixtures';
 import DataTable from './DataTable';
 
-const Template: Story<IDataTableProps> = args => <DataTable {...args}/>;
+const Template: Story<IDataTableProps<IDataTableFixture>> = args => <DataTable {...args} />;
 
 export const DataTableStory = Template.bind({});
 
-DataTableStory.args = {};
+DataTableStory.args = {
+    rows: generateDataTableRows(50),
+    columns,
+    getRowKey
+};
 
 export const argTypes = {};
--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,11 +1,55 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { IDataTableProps } from './DataTable.types';
-import './DataTable.scss'
+import './DataTable.scss';
 
-const DataTable: FC<IDataTableProps> = ({ children }): JSX.Element => {
+export default function DataTable<R>({ columns, rows, getRowKey }: IDataTableProps<R>): JSX.Element {
     return (
-        <div>{ children }</div>
-    );
-};
+        <table className="mc-datatable__table">
+            <thead>
+                <tr>
+                    {columns.map(column => (
+                        <th key={`header-cell-${column.key as string}`}>{column.title}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {rows.map(row => {
+                    const rowKey = getRowKey(row);
 
-export default DataTable;
+                    return (
+                        <tr key={`row-${rowKey}`}>
+                            {columns.map(column => (
+                                <td key={`row-${rowKey}-cell-${column.key as string}`}>
+                                    {column.render ? column.render(row, column.key) : row[column.key]}
+                                </td>
+                            ))}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
+}
+
+/**
+ <DataTable>
+ <DataTableHeader isSticky />
+ <DataTableBody />
+ <DataTablePagination />
+ </DataTable>
+ **/
+
+/**
+ * - идентификатор для строки
+ * - кастомный рендер для ячейки
+ * - кастомный рендер для строки
+ * - раскрываемая строка
+ * - строка с чек-боксом
+ * - кликабельная строка
+ * - редактируемая ячейка
+ * - ячейка с числом
+ */
+
+/**
+ * PagedDataTable - таблица со встроенной пагинацией
+ */
