e624a1b5612b73577ef8de5b7adf4dd98c9b1321
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-02-13 14:30:29 +0500

    add ref to <TextInput />

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { forwardRef } from 'react';
 import cn from 'classnames';
 import { ITextInputProps, TTextInputSize } from './TextInput.types';
 import './TextInput.scss';
@@ -9,43 +9,45 @@ function getSizeClassName(size: TTextInputSize): string {
     return size !== 'm' ? `${blockClassName}--${size}` : '';
 }
 
-const TextInput: FC<ITextInputProps> = ({
-    className,
-    type = 'text',
-    size = 'm',
-    isDisabled,
-    isInvalid,
-    isValid,
-    icon,
-    ...props
-}: ITextInputProps) => {
-    if (isInvalid && isValid) {
-        throw new Error('The properties `isValid` and `isInvalid` can not be true in the same time');
-    }
+const TextInput = forwardRef<HTMLInputElement, ITextInputProps>(
+    ({ className, type = 'text', size = 'm', isDisabled, isInvalid, isValid, icon, ...props }, ref) => {
+        if (isInvalid && isValid) {
+            throw new Error('The properties `isValid` and `isInvalid` can not be true in the same time');
+        }
 
-    const textAreaClassName = cn(
-        'mc-text-input',
-        className,
-        getSizeClassName(size),
-        isInvalid && 'is-invalid',
-        isValid && 'is-valid',
-        icon && 'mc-left-icon-input__input',
-    );
-
-    if (icon) {
-        return (
-            <div className="mc-left-icon-input">
-                <span className="mc-left-icon-input__icon">
-                    {icon}
-                </span>
-                <input className={textAreaClassName} {...props} type={type} aria-invalid={isInvalid} disabled={isDisabled} />
-            </div>
+        const textAreaClassName = cn(
+            'mc-text-input',
+            className,
+            getSizeClassName(size),
+            isInvalid && 'is-invalid',
+            isValid && 'is-valid',
+            icon && 'mc-left-icon-input__input'
         );
-    }
 
-    return (
-        <input className={textAreaClassName} {...props} type={type} aria-invalid={isInvalid} disabled={isDisabled} />
-    );
-};
+        const input = (
+            <input
+                className={textAreaClassName}
+                {...props}
+                ref={ref}
+                type={type}
+                aria-invalid={isInvalid}
+                disabled={isDisabled}
+            />
+        );
+
+        if (icon) {
+            return (
+                <div className="mc-left-icon-input">
+                    <span className="mc-left-icon-input__icon">{icon}</span>
+                    {input}
+                </div>
+            );
+        }
+
+        return input;
+    }
+);
+
+TextInput.displayName = 'MozaicTextInput';
 
 export default TextInput;
