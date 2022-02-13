ce0a3ebb206c30884988bbc58547c9009700c66b
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-02-13 21:50:00 +0500

    add ref to <Toggle />

--- src/components/Toggle/partials/Indicator.tsx
+++ src/components/Toggle/partials/Indicator.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import cn from 'classnames';
 import { IToggleIndicatorProps } from '../Toggle.types';
 
-const ToggleIndicator = ({ className, isChecked, isDisabled, ...props }: IToggleIndicatorProps): JSX.Element => (
-    <input
-        className={cn('mc-toggle__input', className)}
-        {...props}
-        type="checkbox"
-        checked={isChecked}
-        disabled={isDisabled}
-    />
+const ToggleIndicator = forwardRef<HTMLInputElement, IToggleIndicatorProps>(
+    ({ className, isChecked, isDisabled, ...props }, ref) => (
+        <input
+            className={cn('mc-toggle__input', className)}
+            {...props}
+            ref={ref}
+            type="checkbox"
+            checked={isChecked}
+            disabled={isDisabled}
+        />
+    )
 );
+
+ToggleIndicator.displayName = 'MozaicToggleIndicator';
 
 export default ToggleIndicator;
