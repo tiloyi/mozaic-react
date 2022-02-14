104a3dedd4aa0af9261b7c597a451c683bb503db
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-02-14 16:58:28 +0500

    Radio. WIP

--- src/components/Radio/Radio.stories.tsx
+++ src/components/Radio/Radio.stories.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Story } from '@storybook/react';
-import Radio from './Radio';
+import { action } from '@storybook/addon-actions';
+import Button from '../Button';
+import View from '../View';
 import { IRadioProps } from './Radio.types';
+import Radio from './Radio';
 
 const ControlsTemplate: Story<IRadioProps> = args => <Radio {...args}>Radio label</Radio>;
 
@@ -12,3 +16,18 @@ Controls.args = {
     isDisabled: false,
     isInvalid: false
 };
+
+const ReactHookFormTemplate: Story = () => {
+    const { register, handleSubmit } = useForm();
+
+    return (
+        <form onSubmit={handleSubmit(action('Submit'))}>
+            <View marginBottom="mu100">
+                <Radio {...register('value')}>Label</Radio>
+            </View>
+            <Button type="submit">Submit</Button>
+        </form>
+    );
+};
+
+export const ReactHookForm = ReactHookFormTemplate.bind({});
--- src/components/Radio/Radio.tests.tsx
+++ src/components/Radio/Radio.tests.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { useForm } from 'react-hook-form';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Button from '../Button';
+import View from '../View';
 import Radio from './Radio';
+
+interface IFormValues {
+    isChecked: boolean;
+}
+
+interface IFormProps {
+    defaultValues: IFormValues;
+    onSubmit: (values: IFormValues) => void;
+}
+
+const Form = ({ defaultValues, onSubmit }: IFormProps): JSX.Element => {
+    const { register, handleSubmit } = useForm<IFormValues>({ defaultValues });
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <View marginBottom="mu100">
+                <Radio {...register('isChecked')}>Label</Radio>
+            </View>
+            <Button type="submit">Submit</Button>
+        </form>
+    );
+};
 
 describe('components/Radio', () => {
     test('renders children correctly', () => {
@@ -43,5 +69,26 @@ describe('components/Radio', () => {
         render(<Radio isInvalid>Invalid radio label</Radio>);
 
         expect(screen.getByRole('radio')).toHaveClass('is-invalid');
+    });
+
+    test('works with react-hook-form', async () => {
+        const onSubmit = jest.fn();
+        const defaultValues = {
+            isChecked: true
+        };
+
+        render(<Form defaultValues={defaultValues} onSubmit={onSubmit} />);
+
+        screen.debug();
+
+        expect(screen.getByRole('radio')).toBeChecked();
+        //
+        // userEvent.click(screen.getByRole('radio'));
+        //
+        // expect(screen.getByRole('radio')).not.toBeChecked();
+        //
+        // await waitFor(() => userEvent.click(screen.getByRole('button', { name: 'Submit' })));
+        //
+        // expect(onSubmit).toHaveBeenCalledWith({ isChecked: false }, expect.any(Object));
     });
 });
