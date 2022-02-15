dbe0af887e43a1eba97b7b0025dff8a10bc53c86
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-02-15 18:50:24 +0500

    add tests

--- src/components/QuantitySelector/QuantitySelector.tests.tsx
+++ src/components/QuantitySelector/QuantitySelector.tests.tsx
@@ -1,6 +1,38 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { useController, useForm } from 'react-hook-form';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Button from '../Button';
+import View from '../View';
 import QuantitySelector from './QuantitySelector';
+
+interface IFormValues {
+    value: number;
+}
+
+interface IFormProps {
+    defaultValues: IFormValues;
+    onSubmit: (values: IFormValues) => void;
+}
+
+const Form = ({ defaultValues, onSubmit }: IFormProps): JSX.Element => {
+    const { control, handleSubmit } = useForm<IFormValues>({
+        defaultValues
+    });
+
+    const {
+        field: { value, onChange }
+    } = useController({ control, name: 'value' });
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <View marginBottom="mu100">
+                <QuantitySelector placeholder="quantity" value={value} onChange={onChange} />
+            </View>
+            <Button type="submit">Submit</Button>
+        </form>
+    );
+};
 
 describe('components/QuantitySelector', () => {
     test('renders correctly', () => {
@@ -118,5 +150,24 @@ describe('components/QuantitySelector', () => {
         fireEvent.change(screen.getByPlaceholderText('Placeholder'), { target: { value: 11 } });
 
         expect(onChange).not.toBeCalled();
+    });
+
+    test('works with react-hook-form', async () => {
+        const onSubmit = jest.fn();
+        const defaultValues = {
+            value: 0
+        };
+
+        render(<Form defaultValues={defaultValues} onSubmit={onSubmit} />);
+
+        expect(screen.getByPlaceholderText('quantity')).toHaveDisplayValue('0');
+
+        userEvent.click(screen.getByRole('button', { name: 'Increment' }));
+
+        expect(screen.getByPlaceholderText('quantity')).toHaveDisplayValue('1');
+
+        await waitFor(() => userEvent.click(screen.getByRole('button', { name: 'Submit' })));
+
+        expect(onSubmit).toHaveBeenCalledWith({ value: 1 }, expect.any(Object));
     });
 });
