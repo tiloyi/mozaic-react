2363b3176de28703e9f8612c22ffece2fdf21e7c
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-08-10 17:47:34 +0500

    feat(accordion): apply partials

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 import cn from 'classnames';
 import { IAccordionProps } from './Accordion.types';
+import { AccordionContainer, AccordionContent } from './partials';
 import './Accordion.scss';
 
-const blockClassName = 'mc-accordion';
-
-const Accordion: React.FC<IAccordionProps> = ({ className = '', size = 'm', title, isDefaultOpen = false, isDisabled = false, icon, children, ...props }) => {
+const Accordion: React.FC<IAccordionProps> = ({
+    className,
+    size = 'm',
+    title,
+    isDefaultOpen = false,
+    isDisabled = false,
+    icon,
+    children,
+    ...props
+}) => {
     const [isOpen, setIsOpen] = useState(isDefaultOpen);
-    const wrapperClassName = cn(`${blockClassName}`, `${blockClassName}--${size}`, className, {
+
+    const wrapperClassName = cn(className, {
         'is-open': isOpen
     });
 
     return (
-        <div className={wrapperClassName} data-testid='accordion-wrapper' {...props}>
+        <AccordionContainer className={wrapperClassName} size={size} {...props}>
             <div className="mc-accordion__header">
                 <h2 className="mc-accordion__title">
                     <button
@@ -23,22 +32,16 @@ const Accordion: React.FC<IAccordionProps> = ({ className = '', size = 'm', titl
                         data-mc-component="accordion"
                         onClick={() => setIsOpen(!isOpen)}
                         disabled={isDisabled}
-                        data-testid='accordion-button'
                     >
                         {icon}
                         {title}
                     </button>
                 </h2>
             </div>
-            <div
-                id="content"
-                className="mc-accordion__content"
-                aria-hidden={isOpen && 'false'}
-                aria-labelledby="accordion"
-            >
+            <AccordionContent id="content" aria-hidden={isOpen && 'false'} aria-labelledby="accordion">
                 {children}
-            </div>
-        </div>
+            </AccordionContent>
+        </AccordionContainer>
     );
 };
 
--- src/components/Accordion/Accordion.types.ts
+++ src/components/Accordion/Accordion.types.ts
@@ -1,8 +1,19 @@
 import { HTMLAttributes } from 'react';
+
 export const types = ['text', 'removable'] as const;
+
 export const sizes = ['s', 'm'] as const;
 
 export type TAccordionSize = typeof sizes[number];
+
+export interface IAccordionContainer extends HTMLAttributes<HTMLDivElement> {
+    className?: string;
+    size?: TAccordionSize;
+}
+
+export interface IAccordionContent extends HTMLAttributes<HTMLDivElement> {
+    className?: string;
+}
 
 export interface IAccordionProps extends HTMLAttributes<HTMLDivElement> {
     className?: string;
