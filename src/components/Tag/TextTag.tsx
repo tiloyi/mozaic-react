2db3c16b1d11978dd549874764cda70d3b2af1d1
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-08-11 23:37:06 +0500

    add text tag

---
+++ src/components/Tag/TextTag.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+import cn from 'classnames';
+import { ITextTagProps, TTagSize, TTagTheme } from './Tag.types';
+
+export function getSizeModifier(size: TTagSize): string {
+    if (size === 's') {
+        return `mc-tag-text--s`;
+    }
+
+    return '';
+}
+
+export function getThemeModifier(theme: TTagTheme): string {
+    if (theme === 'dark') {
+        return `mc-tag-text--dark`;
+    }
+
+    return '';
+}
+
+const TextTag: FC<ITextTagProps> = ({ className, children, size = 'm', theme = 'light', ...props }) => (
+    <span className={cn('mc-tag-text', getSizeModifier(size), getThemeModifier(theme), className)} {...props}>
+        <span className="mc-tag-text__label">{children}</span>
+    </span>
+);
+
+export default TextTag;
