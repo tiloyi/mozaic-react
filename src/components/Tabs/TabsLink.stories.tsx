f31de01ad0eca793249180554e4edf15db628a2d
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-08-14 14:35:56 +0500

    add tab link stories

---
+++ src/components/Tabs/TabsLink.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Story } from '@storybook/react';
+import { ITabsListProps } from './Tabs.types';
+import TabsLink from './partials/Link';
+import TabsList from './TabsList';
+
+export const LinksTemplate: Story<ITabsListProps> = args => (
+    <TabsList {...args}>
+        <TabsLink href="https://google.com" isSelected>
+            Google
+        </TabsLink>
+        <TabsLink href="https://facebook.com">Facebook</TabsLink>
+        <TabsLink href="https://apple.com">Apple</TabsLink>
+        <TabsLink href="https://microsoft.com" isDisabled>
+            Microsoft
+        </TabsLink>
+    </TabsList>
+);
+
+export const Links = LinksTemplate.bind({});
+
+Links.args = {
+    theme: 'primary-01',
+    width: 'fit'
+};
+
+export const argTypes = {
+    theme: {
+        control: {
+            type: 'select'
+        }
+    },
+    width: {
+        control: {
+            type: 'select'
+        }
+    }
+};
--- src/components/Tabs/partials/Link.tsx
+++ src/components/Tabs/partials/Link.tsx
@@ -1,10 +1,27 @@
 import React, { FC } from 'react';
+import cn from 'classnames';
 import { ITabsLinkProps } from '../Tabs.types';
 
-const TabsLink: FC<ITabsLinkProps> = ({ children, ...props }) => {
+const TabsLink: FC<ITabsLinkProps> = ({ children, isDisabled, isSelected, ...props }) => {
+    const linkClassName = cn(
+        'mc-tabs__link',
+        isDisabled && 'mc-tabs__link--disabled',
+        isSelected && 'mc-tabs__link--selected'
+    );
+
+    if (isDisabled) {
+        return (
+            <li className="mc-tabs__item" role="presentation">
+                <span className={linkClassName} role="tab" aria-selected={isSelected} {...props}>
+                    {children}
+                </span>
+            </li>
+        );
+    }
+
     return (
         <li className="mc-tabs__item" role="presentation">
-            <a className="mc-tabs__link" role="tab" {...props}>
+            <a className={linkClassName} role="tab" aria-selected={isSelected} {...props}>
                 {children}
             </a>
         </li>
