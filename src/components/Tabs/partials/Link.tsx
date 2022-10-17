98f3bd7ef21b5a195116a473055c87ef8658716d
Trésor ILOYI <tresor.iloyi@gmail.com>
2022-10-17 10:55:22 +0200

    feat(component/tabs): update Tabs HTML structure

--- src/components/Tabs/partials/Link.tsx
+++ src/components/Tabs/partials/Link.tsx
@@ -4,16 +4,16 @@ import { ITabsLinkProps } from '../Tabs.types';
 
 const TabsLink: FC<ITabsLinkProps> = ({ children, isDisabled, isSelected, ...props }) => {
     const linkClassName = cn(
-        'mc-tabs__link',
-        isDisabled && 'mc-tabs__link--disabled',
-        isSelected && 'mc-tabs__link--selected'
+        'mc-tabs__element',
+        isDisabled && 'mc-tabs__element--disabled',
+        isSelected && 'mc-tabs__element--selected'
     );
 
     if (isDisabled) {
         return (
             <li className="mc-tabs__item" role="presentation">
                 <span className={linkClassName} role="tab" aria-selected={isSelected} {...props}>
-                    {children}
+                    <span className="mc-tabs__text">{children}</span>
                 </span>
             </li>
         );
@@ -22,7 +22,7 @@ const TabsLink: FC<ITabsLinkProps> = ({ children, isDisabled, isSelected, ...pro
     return (
         <li className="mc-tabs__item" role="presentation">
             <a className={linkClassName} role="tab" aria-selected={isSelected} {...props}>
-                {children}
+                <span className="mc-tabs__text">{children}</span>
             </a>
         </li>
     );
