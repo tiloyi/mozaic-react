16a132eef6da453019a8ecfe998b419c8025a8c8
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-11-01 20:49:21 +0500

    feat(View): implement new spaces

--- src/components/View/View.tsx
+++ src/components/View/View.tsx
@@ -7,16 +7,18 @@ export function sanitize(unit: TMagicUnit): string {
     return unit.replace(/^mu/, '');
 }
 
-function getSpacingClassNames(
+function getVerticalSpacingClassNames(
     prefix: 'p' | 'm',
-    spacing?: TMagicUnit,
     top?: TMagicUnit,
-    right?: TMagicUnit,
     bottom?: TMagicUnit,
-    left?: TMagicUnit
+    vertical?: TMagicUnit
 ): Array<string> {
-    if (spacing) {
-        return [`mu-${prefix}-${sanitize(spacing)}`];
+    if (vertical) {
+        return [`mu-${prefix}v-${sanitize(vertical)}`];
+    }
+
+    if (top === bottom && top !== undefined) {
+        return [`mu-${prefix}v-${sanitize(top)}`];
     }
 
     const classNames: Array<string> = [];
@@ -25,12 +27,31 @@ function getSpacingClassNames(
         classNames.push(`mu-${prefix}t-${sanitize(top)}`);
     }
 
-    if (right) {
-        classNames.push(`mu-${prefix}r-${sanitize(right)}`);
-    }
-
     if (bottom) {
         classNames.push(`mu-${prefix}b-${sanitize(bottom)}`);
+    }
+
+    return classNames;
+}
+
+function getHorizontalSpacingClassNames(
+    prefix: 'p' | 'm',
+    right?: TMagicUnit,
+    left?: TMagicUnit,
+    horizontal?: TMagicUnit
+): Array<string> {
+    if (horizontal) {
+        return [`mu-${prefix}h-${sanitize(horizontal)}`];
+    }
+
+    if (right === left && right !== undefined) {
+        return [`mu-${prefix}h-${sanitize(right)}`];
+    }
+
+    const classNames: Array<string> = [];
+
+    if (right) {
+        classNames.push(`mu-${prefix}r-${sanitize(right)}`);
     }
 
     if (left) {
@@ -38,6 +59,34 @@ function getSpacingClassNames(
     }
 
     return classNames;
+}
+
+function getSpacingClassNames(
+    prefix: 'p' | 'm',
+    spacing?: TMagicUnit,
+    top?: TMagicUnit,
+    right?: TMagicUnit,
+    bottom?: TMagicUnit,
+    left?: TMagicUnit,
+    vertical?: TMagicUnit,
+    horizontal?: TMagicUnit
+): Array<string> {
+    if (spacing) {
+        return [`mu-${prefix}-${sanitize(spacing)}`];
+    }
+
+    if (vertical === horizontal && vertical !== undefined) {
+        return [`mu-${prefix}-${sanitize(vertical)}`];
+    }
+
+    if (top === right && right === bottom && bottom === left && left !== undefined) {
+        return [`mu-${prefix}-${sanitize(left)}`];
+    }
+
+    return [
+        ...getVerticalSpacingClassNames(prefix, top, bottom, vertical),
+        ...getHorizontalSpacingClassNames(prefix, right, left, horizontal)
+    ];
 }
 
 function getShadowModifier(shadow?: TShadow): string {
@@ -65,23 +114,47 @@ const View: FC<IViewProps> = ({
     marginRight,
     marginBottom,
     marginLeft,
+    marginVertical,
+    marginHorizontal,
     padding,
     paddingTop,
     paddingRight,
     paddingBottom,
     paddingLeft,
+    paddingVertical,
+    paddingHorizontal,
     shadow,
     radius,
     ...props
 }): JSX.Element => {
     const margins = useMemo(
-        () => getSpacingClassNames('m', margin, marginTop, marginRight, marginBottom, marginLeft),
-        [margin, marginTop, marginRight, marginBottom, marginLeft]
+        () =>
+            getSpacingClassNames(
+                'm',
+                margin,
+                marginTop,
+                marginRight,
+                marginBottom,
+                marginLeft,
+                marginVertical,
+                marginHorizontal
+            ),
+        [margin, marginTop, marginRight, marginBottom, marginLeft, marginVertical, marginHorizontal]
     );
 
     const paddings = useMemo(
-        () => getSpacingClassNames('p', padding, paddingTop, paddingRight, paddingBottom, paddingLeft),
-        [padding, paddingTop, paddingRight, paddingBottom, paddingLeft]
+        () =>
+            getSpacingClassNames(
+                'p',
+                padding,
+                paddingTop,
+                paddingRight,
+                paddingBottom,
+                paddingLeft,
+                paddingVertical,
+                paddingHorizontal
+            ),
+        [padding, paddingTop, paddingRight, paddingBottom, paddingLeft, paddingVertical, paddingHorizontal]
     );
 
     return createElement(
